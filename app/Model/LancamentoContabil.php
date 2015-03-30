9d8ceb555fddb7e02ff9f02bd2a38efcbec53c49
wilianselzlein <wilianselzlein@gmail.com>
2015-03-30 13:35:11 -0300

    Importar mensalidades.

--- app/Model/LancamentoContabil.php
+++ app/Model/LancamentoContabil.php
@@ -46,7 +46,7 @@ class LancamentoContabil extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-		),
+		)/*,
 		'credito_id' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
@@ -77,7 +77,7 @@ class LancamentoContabil extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		/*'documento' => array(
+		'documento' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
 				//'message' => 'Your custom message here',
