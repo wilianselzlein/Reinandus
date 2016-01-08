e831a0809192beb9adaab1f91902fc76542d703c
pedruino <pedro.escobar@live.com>
2016-01-08 16:13:53 -0200

    DateFormatBehavior-beta

--- app/Model/Behavior/DateFormatBehavior.php
+++ app/Model/Behavior/DateFormatBehavior.php
@@ -1,41 +1,59 @@
-<?php 
+<?php
 class DateFormatBehavior extends ModelBehavior {
    //Our  format
    var $dateFormat = 'd/m/Y';
    //datebase Format
    var $databaseFormat = 'Y-m-d';
 
-   public function setup(Model $Model, $config = array()) {      
-      $this->Model = $Model;
+   public $settings;
+
+   public function setup(Model $model, $settings = array()) {
+     parent::setup($model, $settings);
+     //$this->fields = $settings;
+     $columns = $model->getColumnTypes();
+     //debug($columns);
+     foreach($columns as $column => $type){
+        if(($type != 'date') && ($type != 'datetime')) unset($columns[$column]);
+     }
+     //debug($columns);
+     //debug("------------------------------------ teste ".date('m/d/Y h:i:s a', time())." ".$model->name);
+     //debug($this->fields);
+
+     if (!isset($this->settings[$model->alias])) {
+       $this->settings[$model->alias] = $columns;
+     }
+    $this->settings[$model->alias] = array_merge(  $this->settings[$model->alias], (array)$settings);
    }
 
-   function _changeDateFormat($date = null,$dateFormat){        
+   function _changeDateFormat($date = null,$dateFormat){
       $newDate = str_replace("/", "-", $date);
       $time = strtotime($newDate);
          if($time === false){
             return null;
       }else{
-         return date($dateFormat, $time);   
-      }    
+         return date($dateFormat, $time);
+      }
    }
 
-   //This function search an array to get a date or datetime field. 
-   function _changeDate($queryDataConditions , $dateFormat){
+   //This function search an array to get a date or datetime field.
+   function _changeDate($queryDataConditions , $dateFormat, $model){
       if (isset($queryDataConditions)) {
          foreach($queryDataConditions as $key => $value){
             if(is_array($value)){
-               $queryDataConditions[$key] = $this->_changeDate($value,$dateFormat);
+               $queryDataConditions[$key] = $this->_changeDate($value,$dateFormat, $model);
             } else {
-               $columns = $this->Model->getColumnTypes();
+               $columns = $this->settings[$model->alias];
+/*
                //sacamos las columnas que no queremos
                foreach($columns as $column => $type){
                   if(($type != 'date') && ($type != 'datetime')) unset($columns[$column]);
                }
-               //we look for date or datetime fields on database model  
+*/
+               //we look for date or datetime fields on database model
                foreach($columns as $column => $type){
                   if(strstr($key,$column)){
                      if($type == 'datetime') {
-                        $queryDataConditions[$key] = $this->_changeDateFormat($value,$dateFormat.' H:i:s ');                     
+                        $queryDataConditions[$key] = $this->_changeDateFormat($value,$dateFormat.' H:i:s ');
                      }
                      if($type == 'date'){
                         //debug($queryDataConditions[$key]);
@@ -55,18 +73,18 @@ class DateFormatBehavior extends ModelBehavior {
       return $queryDataConditions;
    }
 
-   function beforeFind(Model $Model, $queryData){
-      $queryData['conditions'] = $this->_changeDate($queryData['conditions'] , $this->databaseFormat);
+   function beforeFind(Model $model, $queryData){
+      $queryData['conditions'] = $this->_changeDate($queryData['conditions'] , $this->databaseFormat, $model);
       return $queryData;
    }
 
-   public function afterFind(Model $Model, $results, $primary = false) {
-      $results = $this->_changeDate($results, $this->dateFormat);
+   public function afterFind(Model $model, $results, $primary = false) {
+      $results = $this->_changeDate($results, $this->dateFormat, $model);
       return $results;
    }
 
-   public function beforeSave(Model $Model, $options = array()) {
-      $Model->data = $this->_changeDate($Model->data, $this->databaseFormat);
+   public function beforeSave(Model $model, $options = array()) {
+      $Model->data = $this->_changeDate($Model->data, $this->databaseFormat, $model);
       return true;
    }
 
