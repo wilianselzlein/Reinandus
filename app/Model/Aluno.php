70b9e29618502e841c392f9d86b95574fdc0d88b
Wilian Selzlein <wilianselzlein@gmail.com>
2016-01-20 00:16:46 -0200

    erro salvar senha ao alterar o aluno

--- app/Model/Aluno.php
+++ app/Model/Aluno.php
@@ -218,7 +218,7 @@ var $actsAs = array('DateFormat');
 	);
    
    public function beforeSave($options = array()) {
-      if (isset($this->data[$this->alias]['senha'])) {
+      if ((isset($this->data[$this->alias]['senha'])) && (strlen($this->data[$this->alias]['senha']) <= 20)) {
          $this->data[$this->alias]['senha'] = AuthComponent::password($this->data[$this->alias]['senha']);
       }
       return true;
