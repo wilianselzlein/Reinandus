c62571daefe46406c4ea080e6d7e7545dfb75af1
Wilian Selzlein <wilianselzlein@gmail.com>
2016-07-01 15:38:20 +0000

    Correcoes permissoes usuarios sem nenhuma permissao configurada, ou seja, acessa tudo

--- app/View/Helper/PermissaoRelatedHelper.php
+++ app/View/Helper/PermissaoRelatedHelper.php
@@ -9,11 +9,19 @@ class PermissaoRelatedHelper extends AppHelper {
     	$permissoes = ClassRegistry::init('permissoes');
         $user_id = CakeSession::read('Auth.User');
         $user_id = $user_id['id'];
-            
-        $options = array('conditions' => array('Permissao.User_id' => $user_id, 'Permissao.Index' => false));
+        
+        $options = array('conditions' => array('Permissao.User_id' => $user_id, 'Permissao.Index' => false, 'Permissao.programa_id' => $programa));
         $permissoes = ClassRegistry::init('Permissao');
         $conta = $permissoes->find('count', $options);
-	    return $conta = 0;
+
+        if ($conta > 0)
+	        return true;
+	    else {
+            $options = array('conditions' => array('Permissao.User_id' => $user_id, 'Permissao.programa_id' => $programa));
+            $conta = $permissoes->find('count', $options);
+            return $conta == 0;
+	    }
+	    
 	}
 } 
 
