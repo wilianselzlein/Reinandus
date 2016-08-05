909ea7c7b5fa13cc1e4641c3d94c5f607122f646
Wilian Selzlein <wilianselzlein@gmail.com>
2016-08-05 02:51:49 +0000

    Possibilidade mascaras

--- app/View/Themed/Cakestrap/webroot/js/cfg-currency.js
+++ app/View/Themed/Cakestrap/webroot/js/cfg-currency.js
@@ -1,1 +1,4 @@
 $('.currency').mask('#.##0,00', {reverse: true});
+//$('.currency').mask('0000000,00', {reverse: true});
+//$('.money').mask('000.000.000.000.000,00', {reverse: true});
+//$('.money2').mask("#.##0,00", {reverse: true});
