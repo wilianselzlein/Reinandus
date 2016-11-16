e333418b9d92f77c6ef751dd5ab66fc3a3e580b8
Wilian Selzlein <wilianselzlein@gmail.com>
2016-11-15 23:25:08 -0200

    Classes Boleto Bradesco

---
+++ app/Plugin/Boletos/Vendor/boletophp/boleto_bradesco.php
@@ -0,0 +1,65 @@
+<?php
+// +----------------------------------------------------------------------+
+// | BoletoPhp - Versão Beta                                              |
+// +----------------------------------------------------------------------+
+// | Este arquivo está disponível sob a Licença GPL disponível pela Web   |
+// | em http://pt.wikipedia.org/wiki/GNU_General_Public_License           |
+// | Você deve ter recebido uma cópia da GNU Public License junto com     |
+// | esse pacote; se não, escreva para:                                   |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+
+// +----------------------------------------------------------------------+
+// | Originado do Projeto BBBoletoFree que tiveram colaborações de Daniel |
+// | William Schultz e Leandro Maniezo que por sua vez foi derivado do	  |
+// | PHPBoleto de João Prado Maia e Pablo Martins F. Costa			       	  |
+// | 																	                                    |
+// | Se vc quer colaborar, nos ajude a desenvolver p/ os demais bancos :-)|
+// | Acesse o site do Projeto BoletoPhp: www.boletophp.com.br             |
+// +----------------------------------------------------------------------+
+
+// +----------------------------------------------------------------------+
+// | Equipe Coordenação Projeto BoletoPhp: <boletophp@boletophp.com.br>   |
+// | Desenvolvimento Boleto Bradesco: Ramon Soares						            |
+// +----------------------------------------------------------------------+
+
+
+// ------------------------- DADOS DINÂMICOS DO SEU CLIENTE PARA A GERAÇÃO DO BOLETO (FIXO OU VIA GET) -------------------- //
+// Os valores abaixo podem ser colocados manualmente ou ajustados p/ formulário c/ POST, GET ou de BD (MySql,Postgre,etc)	//
+
+if(!Configure::load('Boletos.boleto_bradesco')){
+	Configure::load('Boletos.BoletoBradesco');
+}
+
+
+$dadosboleto = Set::merge($dadosboleto, Configure::read('Boleto'));
+
+// DADOS DO BOLETO PARA O SEU CLIENTE
+$dias_de_prazo_para_pagamento = $dadosboleto['dias_vencimento'];
+$taxa_boleto = $dadosboleto['taxa'];
+$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006";
+$valor_cobrado = $dadosboleto['valor_cobrado']; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$valor_cobrado = str_replace(",", ".",$valor_cobrado);
+$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+
+$dadosboleto["nosso_numero"] = $dadosboleto['pedido'];  // Nosso numero sem o DV - REGRA: Máximo de 11 caracteres!
+$dadosboleto["numero_documento"] = $dadosboleto["nosso_numero"];	// Num do pedido ou do documento = Nosso numero
+$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+
+// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+$dadosboleto["quantidade"] = "001";
+$dadosboleto["valor_unitario"] = $valor_boleto;
+$dadosboleto["aceite"] = "";
+$dadosboleto["especie"] = "R$";
+$dadosboleto["especie_doc"] = "DS";
+
+// NÃO ALTERAR!
+include("include/funcoes_bradesco.php");
+include("include/layout_bradesco.php");
+?>
