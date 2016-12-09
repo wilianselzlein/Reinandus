16c50d55a72ca6b2b57d4c4588b914041e9f4dc4
Wilian Selzlein <wilianselzlein@gmail.com>
2016-12-09 15:12:52 +0000

    Final estrutura itau

--- app/Controller/Component/IntegracaoBancaria/IntegracaoItauComponent.php
+++ app/Controller/Component/IntegracaoBancaria/IntegracaoItauComponent.php
@@ -81,63 +81,17 @@ class IntegracaoItauComponent extends IntegracaoBaseComponent {
 				$s .= $this->FormatarTexto('', 30); //Avalista
 			}
 			$s .= $this->FormatarNumero('', 4);	//brancos
+			$s .= $this->FormatarNumero($this->Data[$i]['Conta']['dia_desconto'], 2) . date('m') . date('y'); //data de Mora 
+			$s .= $this->FormatarNumero($this->Data[$i]['Conta']['dia_desconto'], 2); //Prazo de Dias
+			$s .= $this->FormatarNumero('', 1);	//brancos
+			$s .= $this->SequencialDoRegistro();
+			$s .= PHP_EOL;
 
-			//---------------------
-			$s .= $this->FormatarNumero('', 5); //agencia de debito
-			$s .= $this->FormatarTexto('', 1); //digito agencia de debito
-			$s .= $this->FormatarNumero('', 5); //razao da conta corrente
-			$s .= $this->FormatarNumero('', 7); //conta corrente
-			$s .= $this->FormatarTexto('', 1); //digito da conta corrente
-			$carteira = $this->Data[$i]['Conta']['carteira'];
-			$s .= $this->FormatarNumero('0', 1) . $this->FormatarNumero($carteira, 3) . $this->FormatarNumero($this->Data[$i]['Conta']['agencia'], 5) . $this->FormatarNumero($this->Data[$i]['Conta']['conta'], 7) . $this->FormatarNumero($this->Data[$i]['Conta']['conta_dig'], 1); //identificacao da empresa
-			
-			$s .= $this->FormatarTexto('237', 3); //código do banco
-			$s .= $this->FormatarTexto('2', 1); //2 = multa 0 = sem multa
-			$s .= $this->FormatarTexto('0200', 4); //2% multa
-			$num = $this->FormatarNumero($this->Data[$i]['Mensalidade']['id'], 10);
-			$s .= $num . $this->FormatarNumero($this->modulo_11($carteira . $num), 1); //Identificação do Titulo no Banco
-			$s .= $this->FormatarNumero('', 1); //digito auto conferencia do numero bancario
-			$s .= $this->FormatarNumero('0', 10); //Desconto bonificação Dia
-			$s .= $this->FormatarTexto(2, 1); //Emissao do Boleto (1 = Banco) (2 = cliente)
-			$s .= $this->FormatarTexto('N', 1); //nao registra e nao emite
-			$s .= $this->FormatarTexto('', 10); //brancos
-			$s .= $this->FormatarTexto('R', 1); //Indicador Rateio Credito
-			$s .= $this->FormatarTexto('', 1); //Enderecamento para aviso do debito
-			$s .= $this->FormatarTexto('', 2); //Brancos
-			$s .= $this->FormatarTexto('01', 2); //Identificação da Ocorrencia - Remessa
-			$s .= $this->FormatarNumero($this->Data[$i]['Mensalidade']['id'], 10); //Numero do Documento
-			$s .= $this->FormatarTexto(date('ddmmyy', strtotime($this->Data[$i]['Mensalidade']['vencimento'])), 6); //Vencimento
-			$s .= $this->FormatarNumero($this->Data[$i]['Mensalidade']['valor'], 13); //Valor
-			$s .= $this->FormatarNumero('0', 3); //Banco Encarregado Cobrança
-			$s .= $this->FormatarNumero('0', 5); //Agência Depositária
-			$s .= $this->FormatarTexto('01', 2); //Especie de Titulo - Duplicata
-			$s .= $this->FormatarTexto('N', 1); //Identificacao Sempre N
-			
-			$s .= $this->FormatarNumero('0', 2); //1ª instrução
-			$s .= $this->FormatarNumero('0', 2); //2ª instrução
-			$s .= $this->FormatarNumero('0', 13); //Mora por Dia de Atraso
-			$s .= $this->FormatarNumero($this->Data[$i]['Conta']['dia_desconto'], 2) . date('m') . date('y'); //data limite desconto
-			$s .= $this->FormatarNumero($this->Data[$i]['Mensalidade']['desconto'], 13); //Valor descont
-			$s .= $this->FormatarNumero('0', 13); //IOF
-			$s .= $this->FormatarNumero('0', 13); //Abatimento
-			if ($this->Data[$i]['Responsavel']['Id'] > 0) {//Tipo Inscricao Pagador 01 - CPF 02 - CNPJ 03 - PIS/PASEP 98 - Não tem 99 - Outros X
-				$s .= $this->FormatarNumero('02', 2); //CNPJ
-				$s .= $this->FormatarCNPJ($this->Data[$i]['Responsavel']['cnpjcpf'], 14); //CNPJ
-				$s .= $this->FormatarTexto($this->Data[$i]['Responsavel']['razaosocial'], 40); //Nome do Pagador
-				$s .= $this->FormatarTexto($this->Data[$i]['Responsavel']['endereco'] . ' ' . $this->Data[$i]['Responsavel']['numero'] . ' ' . $this->Data[$i]['Responsavel']['bairro'] , 40); //Endereco Pagador
-				$s .= $this->FormatarTexto('', 12); //1 mensagem
-				$s .= $this->FormatarNumero(substr($this->Data[$i]['Responsavel']['cep'], 0, 5), 5); //CEP
-				$s .= $this->FormatarNumero(substr($this->Data[$i]['Responsavel']['cep'], -3), 3); //Sufixo CEP
-			} else {
-				$s .= $this->FormatarNumero('01', 2); //CPF
-				$s .= $this->FormatarCPF($this->Data[$i]['Aluno']['cpf'], 14); //CPF
-				$s .= $this->FormatarTexto($this->Data[$i]['Aluno']['nome'], 40); //Nome do Pagador
-				$s .= $this->FormatarTexto($this->Data[$i]['Aluno']['endereco'] . ' ' . $this->Data[$i]['Aluno']['numero'] . ' ' . $this->Data[$i]['Aluno']['bairro'] , 40); //Endereco Pagador
-				$s .= $this->FormatarTexto('', 12); //1 mensagem
-				$s .= $this->FormatarNumero(substr($this->Data[$i]['Aluno']['cep'], 0, 5), 5); //CEP
-				$s .= $this->FormatarNumero(substr($this->Data[$i]['Aluno']['cep'], -3), 3); //Sufixo CEP
-			}
-			$s .= $this->FormatarTexto('', 60); //2 mensagem
+			$s .= '2';
+			$s .= $this->FormatarTexto('2', 1); //2 = multa em % 1 = multa em R$ 0 = sem multa
+			$s .= $this->FormatarTexto(date('ddmmyy', strtotime($this->Data[$i]['Mensalidade']['vencimento'])), 6); //data multa
+			$s .= $this->FormatarTexto('2', 13); //2% multa
+			$s .= $this->FormatarNumero('', 370); //brancos
 			$s .= $this->SequencialDoRegistro();
 			$s .= PHP_EOL;
 		}
