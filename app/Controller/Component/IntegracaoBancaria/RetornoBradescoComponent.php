28f3a671517698dda96d51869d75c0f2e37f89f7
Wilian Selzlein <wilianselzlein@gmail.com>
2017-02-26 09:42:34 -0300

    Forma pagamento itau

--- app/Controller/Component/IntegracaoBancaria/RetornoBradescoComponent.php
+++ app/Controller/Component/IntegracaoBancaria/RetornoBradescoComponent.php
@@ -65,44 +65,45 @@ class RetornoBradescoComponent extends RetornoBaseComponent {
 	private function MotivosRejeite($codigo) {
 		$retorno = '';
 		switch ((int) $codigo) {
-		    case 0:  $retorno = 'Retorno zero'; break;
-		    case 2:  $retorno = 'Código do registro detalhe inválido'; break;
-		    case 3:  $retorno = 'Código da ocorrência inválida'; break;
-	        case 4:  $retorno = 'Código de ocorrência não permitida para a carteira'; break;
-	        case 5:  $retorno = 'Código de ocorrência não numérico'; break;
-	        case 7:  $retorno = 'Agência/conta/Digito Inválido'; break;
-	        case 8:  $retorno = 'Nosso número inválido'; break;
-	        case 9:  $retorno = 'Nosso número duplicado'; break;
-	        case 10: $retorno = 'Carteira inválida'; break;
-	        case 13: $retorno = 'Identificação da emissão do bloqueto inválida (Novo)'; break;
-	        case 16: $retorno = 'Data de vencimento invalida'; break;
-	        case 18: $retorno = 'Vencimento fora do prazo de operação'; break;
-	        case 20: $retorno = 'Valor do Título inválido'; break;
-	        case 21: $retorno = 'Espécie do Título inválida'; break;
-	        case 22: $retorno = 'Espécie não permitida para a carteira'; break;
-	        case 24: $retorno = 'Data de emissão inválida'; break;
-	        case 28: $retorno = 'Código do desconto inválido (Novo)'; break;
-	        case 38: $retorno = 'Prazo para protesto inválido'; break;
-	        case 44: $retorno = 'Agência Beneficiárionão prevista'; break;
-	        case 45: $retorno = 'Nome do pagador não informado (Novo)'; break;
-	        case 46: $retorno = 'Tipo/número de inscrição do pagador inválidos (Novo)'; break;
-	        case 47: $retorno = 'Endereço do pagador não informado (Novo)'; break;
-	        case 48: $retorno = 'CEP Inválido (Novo)'; break;
-	        case 50: $retorno = 'CEP irregular Banco Correspondente'; break;
-	        case 63: $retorno = 'Entrada para Título já cadastrado'; break;
-	        case 65: $retorno = 'Limite excedido (Novo)'; break;
-	        case 66: $retorno = 'Número autorização inexistente (Novo)'; break;
-	        case 68: $retorno = 'Débito não agendado - erro nos dados de remessa'; break;
-	        case 69: $retorno = 'Débito não agendado - Pagador não consta no cadastro de autorizante'; break;
-	        case 70: $retorno = 'Débito não agendado - Beneficiário não autorizado pelo Pagador'; break;
-	        case 71: $retorno = 'Débito não agendado - Beneficiário não participa do débito Automático'; break;
-	        case 72: $retorno = 'Débito não agendado - Código de moeda diferente de R$'; break;
-	        case 73: $retorno = 'Débito não agendado - Data de vencimento inválida '; break;
-	        case 74: $retorno = 'Débito não agendado - Conforme seu pedido, Título não registrado'; break;
-	        case 75: $retorno = 'Débito não agendado – Tipo de número de inscrição do debitado inválido'; break;
+			case 0:  $retorno = 'Retorno zero'; break;
+			case 2:  $retorno = 'Código do registro detalhe inválido'; break;
+			case 3:  $retorno = 'Código da ocorrência inválida'; break;
+			case 4:  $retorno = 'Código de ocorrência não permitida para a carteira'; break;
+			case 5:  $retorno = 'Código de ocorrência não numérico'; break;
+			case 7:  $retorno = 'Agência/conta/Digito Inválido'; break;
+			case 8:  $retorno = 'Nosso número inválido'; break;
+			case 9:  $retorno = 'Nosso número duplicado'; break;
+			case 10: $retorno = 'Carteira inválida'; break;
+			case 13: $retorno = 'Identificação da emissão do bloqueto inválida (Novo)'; break;
+			case 16: $retorno = 'Data de vencimento invalida'; break;
+			case 18: $retorno = 'Vencimento fora do prazo de operação'; break;
+			case 20: $retorno = 'Valor do Título inválido'; break;
+			case 21: $retorno = 'Espécie do Título inválida'; break;
+			case 22: $retorno = 'Espécie não permitida para a carteira'; break;
+			case 24: $retorno = 'Data de emissão inválida'; break;
+			case 28: $retorno = 'Código do desconto inválido (Novo)'; break;
+			case 38: $retorno = 'Prazo para protesto inválido'; break;
+			case 44: $retorno = 'Agência Beneficiárionão prevista'; break;
+			case 45: $retorno = 'Nome do pagador não informado (Novo)'; break;
+			case 46: $retorno = 'Tipo/número de inscrição do pagador inválidos (Novo)'; break;
+			case 47: $retorno = 'Endereço do pagador não informado (Novo)'; break;
+			case 48: $retorno = 'CEP Inválido (Novo)'; break;
+			case 50: $retorno = 'CEP irregular Banco Correspondente'; break;
+			case 63: $retorno = 'Entrada para Título já cadastrado'; break;
+			case 65: $retorno = 'Limite excedido (Novo)'; break;
+			case 66: $retorno = 'Número autorização inexistente (Novo)'; break;
+			case 68: $retorno = 'Débito não agendado - erro nos dados de remessa'; break;
+			case 69: $retorno = 'Débito não agendado - Pagador não consta no cadastro de autorizante'; break;
+			case 70: $retorno = 'Débito não agendado - Beneficiário não autorizado pelo Pagador'; break;
+			case 71: $retorno = 'Débito não agendado - Beneficiário não participa do débito Automático'; break;
+			case 72: $retorno = 'Débito não agendado - Código de moeda diferente de R$'; break;
+			case 73: $retorno = 'Débito não agendado - Data de vencimento inválida '; break;
+			case 74: $retorno = 'Débito não agendado - Conforme seu pedido, Título não registrado'; break;
+			case 75: $retorno = 'Débito não agendado – Tipo de número de inscrição do debitado inválido'; break;
 		}
 		return $retorno;
 	}
+
 }
 
 ?>
--- app/Controller/Component/IntegracaoBancaria/RetornoItauComponent.php
+++ app/Controller/Component/IntegracaoBancaria/RetornoItauComponent.php
@@ -49,16 +49,51 @@ class RetornoItauComponent extends RetornoBaseComponent {
 		$linha = [];
 		
 		$linha['linha'] = $this->Linha;
-		$linha['id'] = substr($this->Linha, 38, 24);
+		$linha['id'] = trim(substr($this->Linha, 37, 24));
+		$linha['confirmado'] = trim(substr($this->Linha, 108, 2));
 		$linha['pagamento'] = $this->Pagamento();
 		$linha['acrescimo'] = $this->FormatarValor(substr($this->Linha, 189, 13)) + $this->FormatarValor(substr($this->Linha, 202, 13)) + $this->FormatarValor(substr($this->Linha, 267, 13));
 		$linha['desconto'] = $this->FormatarValor(substr($this->Linha, 241, 13));
 		$linha['pago'] = $this->FormatarValor(substr($this->Linha, 254, 13));
 		$linha['formapgto_id'] = $this->FormaPgto_Id;
 		$linha['documento'] = $this->Arquivo;
+		$linha['aluno'] = trim(substr($this->Linha, 324, 30));
+		$linha['formapagamento'] = $this->FormaPagamento(substr($this->Linha, 392, 2));
 
-		debug($linha); die;
+		//debug($linha); die;
 		$validacoes[] = $linha;
+	}
+
+	private function FormaPagamento($codigo) {
+		$retorno = '';
+		switch ((string) $codigo) {
+			case 'AA': $retorno = 'CAIXA ELETRÔNICO BANCO ITAÚ DISPONÍVEL'; break;
+			case 'AC': $retorno = 'PAGAMENTO EM CARTÓRIO AUTOMATIZADO A COMPENSAR'; break;
+			case 'AO': $retorno = 'ACERTO ONLINE DISPONÍVEL'; break;
+			case 'BC': $retorno = 'BANCOS CORRESPONDENTES DISPONÍVEL'; break;
+			case 'BF': $retorno = 'ITAÚ BANKFONE DISPONÍVEL'; break;
+			case 'BL': $retorno = 'ITAÚ BANKLINE DISPONÍVEL'; break;
+			case 'B0': $retorno = 'OUTROS BANCOS – RECEBIMENTO OFF-LINE A COMPENSAR'; break;
+			case 'B1': $retorno = 'OUTROS BANCOS – PELO CÓDIGO DE BARRAS A COMPENSAR'; break;
+			case 'B2': $retorno = 'OUTROS BANCOS – PELA LINHA DIGITÁVEL A COMPENSAR'; break;
+			case 'B3': $retorno = 'OUTROS BANCOS – PELO AUTO ATENDIMENTO A COMPENSAR'; break;
+			case 'B4': $retorno = 'OUTROS BANCOS – RECEBIMENTO EM CASA LOTÉRICA A COMPENSAR'; break;
+			case 'B5': $retorno = 'OUTROS BANCOS – CORRESPONDENTE A COMPENSAR'; break;
+			case 'B6': $retorno = 'OUTROS BANCOS – TELEFONE A COMPENSAR'; break;
+			case 'B7': $retorno = 'OUTROS BANCOS – ARQUIVO ELETRÔNICO (Pagamento Efetuado por meio de troca de arquivos) A COMPENSAR '; break;
+			case 'CC': $retorno = 'AGÊNCIA ITAÚ – COM CHEQUE DE OUTRO BANCO ou (CHEQUE ITAÚ)* A COMPENSAR'; break;
+			case 'CI': $retorno = 'CORRESPONDENTE ITAÚ DISPONÍVEL'; break;
+			case 'CK': $retorno = 'SISPAG – SISTEMA DE CONTAS A PAGAR ITAÚ DISPONÍVEL'; break;
+			case 'CP': $retorno = 'AGÊNCIA ITAÚ – POR DÉBITO EM CONTA CORRENTE, CHEQUE ITAÚ* OU DINHEIRO DISPONÍVEL'; break;
+			case 'DG': $retorno = 'AGÊNCIA ITAÚ – CAPTURADO EM OFF-LINE DISPONÍVEL'; break;
+			case 'LC': $retorno = 'PAGAMENTO EM CARTÓRIO DE PROTESTO COM CHEQUE A COMPENSAR'; break;
+			case 'EA': $retorno = 'TERMINAL DE CAIXA DISPONÍVEL'; break;
+			case 'Q0': $retorno = 'AGENDAMENTO – PAGAMENTO AGENDADO VIA BANKLINE OU OUTRO CANAL ELETRÔNICO E LIQUIDADO NA DATA INDICADA DISPONÍVEL'; break;
+			case 'RA': $retorno = 'DIGITAÇÃO – REALIMENTAÇÃO AUTOMÁTICA DISPONÍVEL'; break;
+			case 'ST': $retorno = 'PAGAMENTO VIA SELTEC** DISPONÍVEL'; break;
+
+		}
+		return $retorno;
 	}
 
 }
