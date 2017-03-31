c0a27580fec72ec367a101449f113fe1c83f0137
Wilian Selzlein <wilianselzlein@gmail.com>
2017-03-30 23:10:23 -0300

    Correcao erro JS calcular desconto

--- app/View/Themed/Cakestrap/webroot/js/Mensalidade-Calc.js
+++ app/View/Themed/Cakestrap/webroot/js/Mensalidade-Calc.js
@@ -2,9 +2,16 @@ $(document).ready( function() {
 
     $('.calc').change(function(){
         var subtotal = parseFloat($('#MensalidadeValor').val().replace(".", "").replace(",", ".")) || 0.00;
-        var acrescimo = parseFloat($('#MensalidadeAcrescimo').val().replace(".", "").replace(",", ".")) || 0.00;
+        var acrescimo = 0.00;
+        if( $('#MensalidadeAcrescimo').length)
+            acrescimo = parseFloat($('#MensalidadeAcrescimo').val().replace(".", "").replace(",", ".")) || 0.00;
+
+        var bolsa = 0.00;
+        if( $('#MensalidadeBolsa').length)
+            bolsa = parseFloat($('#MensalidadeBolsa').val().replace(".", "").replace(",", ".")) || 0.00;
+
         var desconto = parseFloat($('#MensalidadeDesconto').val().replace(".", "").replace(",", ".")) || 0.00;
-        var total = subtotal + acrescimo - desconto;
+        var total = subtotal + acrescimo - bolsa - desconto;
 
         //$('#MensalidadeLiquido').val(total.toFixed(2));
         $('#MensalidadeLiquido').val(total.toFixed(2),2);
